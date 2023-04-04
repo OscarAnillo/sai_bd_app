b9a1db7c330330ea2a3a4b163bbcf0542f200d08
oscar anillo <oscarani14@gmail.com>
2023-04-03 19:12:40 -0500

    add UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
+import { useState } from "react";
+import { TitleComponent } from "./Components/Title-component";
+
+import axios from "axios";
+import "animate.css";
+
 import "./App.css";
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const [data, setData] = useState([]);
+  const [submitted, setSubmitted] = useState(false);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!userInput) {
+      alert("Dame tu nombre!");
+      return;
+    }
+    axios
+      .post("http://localhost:3500/api/guests", {
+        content: userInput,
+      })
+      .then((res) => {
+        setData(data.concat(res.data));
+      })
+      .catch(console.log);
+    setUserInput("");
+    setSubmitted(true);
+  };
+
   return (
     <div className="App">
-      <h1>Oscar Anillo</h1>
-      <p>Fullstack Developer</p>
+      {!submitted ? (
+        <>
+          <TitleComponent />
+          <form
+            onSubmit={submitHandler}
+            className="animate__animated animate__fadeIn"
+          >
+            <input
+              type="text"
+              placeholder="Escribe tu nombre aqui"
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+            />
+            <button>Confirmar</button>
+          </form>
+        </>
+      ) : (
+        <h1 className="thanks animate__animated animate__fadeInLeft">
+          Gracias! <br />
+          Nos vemos allá!
+        </h1>
+      )}
     </div>
   );
 }
---
+++ src/Components/Title-component.js
@@ -0,0 +1,12 @@
+import "animate.css";
+
+export const TitleComponent = () => {
+  return (
+    <div className="title-div animate__animated animate__backInDown">
+      <p>Te invitamos a festejar el cumple de</p>
+      <h1>Saray</h1>
+      <p>8 de abril • 7pm</p>
+      <p>Calle 44 #7g-05, Alboraya (Por la troja de la 8)</p>
+    </div>
+  );
+};
